a8bf4b35531417d70ffa85c2fbb674614d410180
Mochammad Masbuchin <buchin@dropsugar.com>
2012-02-07 21:17:01 +0700

    refactor

--- config.php
+++ config.php
@@ -1,23 +1,8 @@
 <?php
-/**
- * The base configurations of the Noobita.
- * @package Noobita
- */
- 
-//name of your site
-define('SITE_NAME','go');
-
-//description of your site
-define('SITE_DESC','hide referrer &amp; link anonymously');
-
-//Redirection time, in seconds
-define('REDIR', 25);
+//Redirection time, in miliseconds
+define('REDIR', 25000);
 
 //change this to your own theme if you like
 define('THEME','safebrowsing');
 
-
-//stop editing here
-/** Define ABSPATH as this files directory */
-define( 'ABSPATH', dirname(__FILE__) . '/' );
 ?>
